a52b461500cfb5216d374e4cce0cf8fc1e8a69e2
Ed <silva.email@yahoo.com>
2023-12-31 09:33:30 -0500

    Add panic function challenge and solution

---
+++ public/panic_function_challenge.js
@@ -0,0 +1,23 @@
+/**!SECTION 001 - Panic Function Challenge */
+
+/* Panic function 
+Write a PANIC! function. The function should take in a sentence and return the same
+sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
+built in string methods. 
+
+If the string is a phrase or sentence, add a 😱 emoji in between each word. 
+
+Example input: "Hello"
+Example output: "HELLO!"
+
+Example input: "I'm almost out of coffee"
+Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+*/
+
+
+const panicFuntion = (str) =>{
+    
+    return str.split(' ').join('😱 ') 
+}
+console.log(panicFuntion('hello world all of my friends!'))
+
